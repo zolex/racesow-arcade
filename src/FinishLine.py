a1db3e8ba585b5a146f377553ebceef87b004aa1
Andreas Linden <zlx@gmx.de>
2025-08-08 14:48:09 +0200

    fix finish line pixmap

--- src/FinishLine.py
+++ src/FinishLine.py
@@ -12,4 +12,4 @@ class FinishLine(StartLine):
 
     def __init__(self, pos: Vector2):
         super().__init__(pos)
-        self.sprite = pygame.image.load(os.path.join(config.assets_folder, 'graphics', 'finish.png')).convert_alpha()
+        self.sprite = pygame.image.load(os.path.join(config.assets_folder, 'graphics', 'start.png')).convert_alpha()
